a6fd04a3a1656680f9af5aea1ff3b601729f9c33
Ehiorobo christopher Edosa <ehiorobo.chris@gmail.com>
2023-08-25 00:22:52 -0700

    add

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,18 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @a: pointer to the list_t list
+ * @h: pointer to the list_t list
  *
- * Return: number of elements in a
+ * Return: number of elements in h
  */
-size_t list_len(const list_t *a)
+size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t n = 0;
 
- while (a)
- {
- n++;
- a = a->next;
- }
- return (n);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
